ae02bc879c061077cdb39fbeddd2c36844ac7cce
Adrian Blair <adrianblair@code-smart.co.uk>
2021-03-01 14:18:14 +0000

    Changed GameToken::Model documentation and tests

--- lib/game_token/model.rb
+++ lib/game_token/model.rb
@@ -1,16 +1,23 @@
 # frozen_string_literal: true
 
+require 'dry-struct'
+
 # Namespace for game token related modules and classes
 # @author Adrian Blair
 module GameToken
-  # Class representing a game token model structure
+  # Class representing a game token model structure which inherits from {https://dry-rb.org/gems/dry-struct Dry::Struct}
   #
-  # @attr [String] id holds the id of the game token
-  # @attr [String] token_name holds the name of the game token
-  # @attr [String] token_key holds the key of the game token
-  # @attr [Array<String>] token_domains an array of domains of which the
-  # game token validates against
-  class Model
-    attr_accessor :id, :token_name, :token_key, :token_domains
+  # @attr_reader [String] id holds the id of the game token
+  # @attr_reader [String] token_name holds the name of the game token
+  # @attr_reader [String] token_key holds the key of the game token
+  # @attr_reader [Array<String>] token_domains an array of domains of which the game token validates against
+  # @see https://dry-rb.org/gems/dry-struct Dry::Struct
+  class Model < Dry::Struct
+    transform_keys(&:to_sym)
+
+    attribute :id, App::Types::Coercible::String.optional
+    attribute :token_name, App::Types::Coercible::String
+    attribute :token_key, App::Types::Coercible::String
+    attribute :token_domains, App::Types::Array.of(App::Types::Coercible::String)
   end
 end
--- spec/game_token/model_spec.rb
+++ spec/game_token/model_spec.rb
@@ -5,26 +5,35 @@ require 'byebug'
 
 describe GameToken::Model do
   before(:all) do
-    @model = GameToken::Model.new
+    @model = GameToken::Model.new(
+      id: "token's id",
+      token_name: "token's name",
+      token_key: "token's key",
+      token_domains: ['localhost']
+    )
   end
 
   context 'the model has an id attribute' do
     it 'returns the current id' do
+      expect(@model.id).to eql("token's id")
     end
   end
 
   context 'the model has a token name attribute' do
     it 'returns the current token name' do
+      expect(@model.token_name).to eql("token's name")
     end
   end
 
   context 'the model has a token key attribute' do
     it 'returns the current token key' do
+      expect(@model.token_key).to eql("token's key")
     end
   end
 
   context 'the model has a token domains attribute' do
     it 'returns the current token domains' do
+      expect(@model.token_domains).to include('localhost')
     end
   end
 end
