3363ea7f5daf038f34268c34dec098fe96a5ad29
Adrian Blair <adrianblair@code-smart.co.uk>
2021-03-05 10:52:31 +0000

    corrected contract's documentation.

--- lib/game_token/base_game_token_contract.rb
+++ lib/game_token/base_game_token_contract.rb
@@ -7,6 +7,8 @@ require 'dry-validation'
 module GameToken
   # A contract validating data in a new game token
   #
+  # - registers a macro :no_blank_spaces_format where it will fail if spaces are found
+  # - registers a macro :only_unique_array_values where it will fail if repeating values are found
   # @see https://dry-rb.org/gems/dry-validation Dry::Validation
   class BaseGameTokenContract < Dry::Validation::Contract
     register_macro(:no_blank_spaces_format) do
--- lib/game_token/new_game_token_contract.rb
+++ lib/game_token/new_game_token_contract.rb
@@ -7,24 +7,24 @@ require 'dry-validation'
 module GameToken
   # A contract validating data in a new game token
   #
-  # @attr_reader [String] id holds the id of the game token
-  # @attr_reader [String] token_name holds the name of the game token
-  # @attr_reader [String] token_key holds the key of the game token
-  # @attr_reader [Array<String>] token_domains an array of domains of which the game token validates against
+  # - required [String] token_name holds the name of the game token
+  # - required [String] token_key holds the key of the game token
+  # - optional [Array<String>] token_domains an array of domains of which the game token validates against
   # @see https://dry-rb.org/gems/dry-validation Dry::Validation
   class NewGameTokenContract < GameToken::BaseGameTokenContract
     params do
       required(:token_name).filled(:string)
-      # TODO: shouldn't be changeable through a post
-      # TODO: should have no spaces
       required(:token_key).filled(:string)
-      # TODO: should have no spaces
-      # TODO: should be a set so that the values are unique.
       optional(:token_domains).array(:string)
     end
 
     rule(:token_key).validate(:no_blank_spaces_format)
     rule(:token_domains).validate(:only_unique_array_values)
+
+    # This validation is very simple and only to help prevent user error.
+    # In reality if this was a public facing API then the validation would have to be
+    # alot more stringent.
+    # An example would be to use something like @see https://github.com/johno/domain-regex domain-regex.
     rule(:token_domains).each do
       key.failure('cannot contain any blank spaces') if value.match(' ')
     end
