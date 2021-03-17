73e983b8dda5ddbf2888986bacc2c6dd87ed70f1
Adrian Blair <adrianblair@code-smart.co.uk>
2021-03-17 20:43:09 +0000

    updated data_access_object_spec to use let

--- spec/game_token/data_access_object_spec.rb
+++ spec/game_token/data_access_object_spec.rb
@@ -3,17 +3,11 @@
 require 'spec_helper'
 
 describe GameToken::DataAccessObject do
-  before(:each) do
-    @test_kind = 'test_kind'
-    @test_id = 'test_id'
-    @test_return_entity = {}
-
-    @mock = instance_double(Data::DataSource)
-  end
+  let(:data_source) { instance_double(Data::DataSource) }
 
   context 'when initializing a new GameToken::DataAccessObject object' do
     it 'should set the data_kind attribute value' do
-      dao = GameToken::DataAccessObject.new(data_source: @mock)
+      dao = GameToken::DataAccessObject.new(data_source: data_source)
       expect(dao.data_kind).to eql('GameToken')
     end
   end
