53691ecaaeeaa12232ce9e343f7bdfac27b41789
Adrian Blair <adrianblair@code-smart.co.uk>
2021-03-17 20:46:18 +0000

    updated existing_game_token_contract_spec to use let

--- spec/game_token/existing_game_token_contract_spec.rb
+++ spec/game_token/existing_game_token_contract_spec.rb
@@ -3,20 +3,12 @@
 require 'spec_helper'
 
 describe GameToken::ExistingGameTokenContract do
-  before(:all) do
-    @params = {
-      id: "token's id",
-      token_name: "token's name",
-      token_key: 'abc123',
-      token_domains: ['localhost']
-    }
-
-    @contract = GameToken::ExistingGameTokenContract.new
-  end
+  let(:contract) { GameToken::ExistingGameTokenContract.new }
+  let(:game_token_base_hash) { build(:game_token_base_hash) }
 
   context 'when the call method is called with a valid parameters hash' do
     it 'validates game token parameters without errors' do
-      contract_result = @contract.call(@params)
+      contract_result = contract.call(game_token_base_hash)
 
       expect(contract_result.values.count).to eq(4)
       expect(contract_result.errors.count).to eq(0)
@@ -29,7 +21,7 @@ describe GameToken::ExistingGameTokenContract do
 
   context 'when the call method is called with an empty parameters hash' do
     it 'validates game token parameters with errors' do
-      contract_result = @contract.call({})
+      contract_result = contract.call({})
       contract_errors_hash = contract_result.errors.to_h
 
       expect(contract_result.values.count).to eq(0)
