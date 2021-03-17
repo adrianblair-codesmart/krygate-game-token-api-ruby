d333ec98d76be676e17fbcc51dcef907e4806518
Adrian Blair <adrianblair@code-smart.co.uk>
2021-03-17 20:39:20 +0000

    updated data_source_spec to use let

--- spec/data/data_source_spec.rb
+++ spec/data/data_source_spec.rb
@@ -3,43 +3,41 @@
 require 'spec_helper'
 
 describe Data::DataSource do
-  before(:all) do
-    @data_source = Data::DataSource.new
-  end
+  let(:data_source) { Data::DataSource.new }
 
   context 'when the find method is called' do
     it 'responds to the call' do
-      expect(@data_source).to respond_to(:find)
+      expect(data_source).to respond_to(:find)
     end
   end
 
   context 'when the query method is called' do
     it 'responds to the call' do
-      expect(@data_source).to respond_to(:query)
+      expect(data_source).to respond_to(:query)
     end
   end
 
   context 'when the insert method is called' do
     it 'responds to the call' do
-      expect(@data_source).to respond_to(:insert)
+      expect(data_source).to respond_to(:insert)
     end
   end
 
   context 'when the update method is called' do
     it 'responds to the call' do
-      expect(@data_source).to respond_to(:update)
+      expect(data_source).to respond_to(:update)
     end
   end
 
   context 'when the save method is called' do
     it 'responds to the call' do
-      expect(@data_source).to respond_to(:save)
+      expect(data_source).to respond_to(:save)
     end
   end
 
   context 'when the delete method is called' do
     it 'responds to the call' do
-      expect(@data_source).to respond_to(:delete)
+      expect(data_source).to respond_to(:delete)
     end
   end
 end
